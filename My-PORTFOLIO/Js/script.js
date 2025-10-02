111d3146f056e647f23b3cbff4b3c4bdde58fca7
Abu-muaz <plshireseid@gmail.com>
2025-10-02 21:17:38 +0300

    Update script.js

--- My-PORTFOLIO/Js/script.js
+++ My-PORTFOLIO/Js/script.js
@@ -677,11 +677,14 @@ form.addEventListener("submit", async (e) => {
   };
 
   try {
-    const res = await fetch("http://localhost:5000/api/contact", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    });
+    const res = await fetch(
+      "https://portfolio-backend-nsmo.onrender.com/api/contact",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      }
+    );
 
     const data = await res.json();
     
